503baf36b719e50f2941ea7c09b49b7ec75e95fa
Matt Billock <matt@billock.org>
2022-01-25 00:39:48 -0600

    Add deliberately flaky test

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,5 +1,6 @@
 const handler = require('../handler');
 const https = require('https');
+const { env } = require('process');
 
 test('correct response code is returned', () => {
     var old = https.request;
@@ -14,5 +15,10 @@ test('correct response code is returned', () => {
 });
 
 test('forced failure in CI for Thundra demonstration', () => {
+    if(process.env.APP_ENV == "CI") {
+        if(Math.random() < .2) {
+            expect(false).toBe(true);
+        }
+    }
     expect(true).toBe();
 });
